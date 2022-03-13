ca357fdd09e9ef15c084625075475353cdb3575e
Lenovo <Lenovo@DESKTOP-RPKE00A>
2022-03-14 02:58:28 +0530

    initial commit

---
+++ src/main/java/com/vishwanath/springboot/SpringbootDeployAwsApplication.java
@@ -0,0 +1,13 @@
+package com.vishwanath.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeployAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeployAwsApplication.class, args);
+	}
+
+}
